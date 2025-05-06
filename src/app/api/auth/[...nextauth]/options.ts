d1d277e4c77411b1774b96f7b0c862c9d49d27f6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-05-06 17:08:04 -0300

    ✨ feat: integra PrismaAdapter ao NextAuth para persistência de dados

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,14 @@
 import { AuthOptions } from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import GitHubProvider from 'next-auth/providers/github'
+import db from 'prisma/db'
 
 if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET) {
     throw new Error('GITHUB_ID e GITHUB_SECRET devem estar definidos nas variáveis de ambiente.')
 }
 
 export const options: AuthOptions = {
+    adapter: PrismaAdapter(db),
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
