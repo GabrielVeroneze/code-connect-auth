cffa86accee19e1b3c28cd2aa564d2ee74b15c47
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-05-06 17:05:15 -0300

    ✨ feat: tipa objeto de configuração do NextAuth com AuthOptions

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,10 +1,11 @@
+import { AuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 
 if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET) {
     throw new Error('GITHUB_ID e GITHUB_SECRET devem estar definidos nas variáveis de ambiente.')
 }
 
-export const options = {
+export const options: AuthOptions = {
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
