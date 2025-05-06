735588e74c5ac80e0b0f412c2ece5f3aa943a474
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-05-06 20:23:44 -0300

    🔧 chore: atualiza remotePatterns para permitir subdomínios do githubusercontent.com

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'raw.githubusercontent.com',
+                hostname: '*.githubusercontent.com',
                 port: '',
                 pathname: '**',
             },
