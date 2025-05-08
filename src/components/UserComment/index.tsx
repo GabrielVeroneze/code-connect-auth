18b0ca168cc9906982e3f4b5104d67e50f7fc548
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-05-07 23:11:20 -0300

    ♻️ refactor: reutiliza componente Avatar para exibir imagem do autor

--- src/components/UserComment/index.tsx
+++ src/components/UserComment/index.tsx
@@ -1,5 +1,5 @@
+import { Avatar } from '@/components/Avatar'
 import { Comment } from '@/types/Comment'
-import Image from 'next/image'
 import styles from './UserComment.module.css'
 
 interface UserCommentProps {
@@ -8,17 +8,9 @@ interface UserCommentProps {
 }
 
 export const UserComment = ({ comment, tema = 'escuro' }: UserCommentProps) => {
-    const imgSrc = comment.author?.avatar ?? comment.author?.image
-
     return (
         <div className={styles.container}>
-            <Image
-                className={styles.imagem}
-                src={imgSrc ?? '/images/default-avatar.png'}
-                alt={`Avatar do(a) ${comment.author?.name}`}
-                height={32}
-                width={32}
-            />
+            <Avatar author={comment?.author} />
             <strong className={`${styles.nome} ${styles[tema]}`}>
                 @{comment.author?.name}
             </strong>
