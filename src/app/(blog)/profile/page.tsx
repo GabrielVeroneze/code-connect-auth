57b7fd02641b16cae4d0830ee59ff7f6e3656e39
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-05-14 16:31:54 -0300

    ✨ feat: redireciona usuários não autenticados e busca dados atualizados do usuário no banco

--- src/app/(blog)/profile/page.tsx
+++ src/app/(blog)/profile/page.tsx
@@ -1,19 +1,27 @@
 import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 import { options } from '@/app/api/auth/[...nextauth]/options'
 import { ProfileImageUploader } from '@/components/ProfileImageUploader'
+import db from '../../../../prisma/db'
 import styles from './profile.module.css'
 
 const Profile = async () => {
     const session = await getServerSession(options)
 
-    if (!session) {
-        return null
+    if (!session || !session.user) {
+        redirect('/api/auth/signin?callbackUrl=/profile')
     }
+
+    const user = await db.user.findUnique({
+        where: {
+            email: session.user.email,
+        },
+    })
 
     return (
         <section>
             <h1 style={{ color: 'white' }}>Profile</h1>
-            <ProfileImageUploader user={session.user} />
+            <ProfileImageUploader user={user} />
         </section>
     )
 }
